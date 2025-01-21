12973780156375886516debbf4b6022352344d27
1poundSOCKS <mathew.coburn@gmail.com>
2025-01-21 11:31:28 +0000

    use older c++ std

--- source/main.cpp
+++ source/main.cpp
@@ -2,8 +2,10 @@
 
 auto main(int argc, char* argv[]) -> int
 {
-  std::println("{}", argc);
-  std::println("{}", argv[0]);
+  // std::println("{}", argc);
+  // std::println("{}", argv[0]);
+  std::cout << argc << '\n';
+  std::cout << argv[0] << '\n';
 
   return 0;
 }
--- source/pch.h
+++ source/pch.h
@@ -1,1 +1,2 @@
-#include <print>
+// #include <print>
+#include <iostream>
