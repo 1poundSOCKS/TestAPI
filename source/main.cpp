550050119fa713b6d1915e5791fb7b9293422708
1poundSOCKS <mathew.coburn@gmail.com>
2025-01-22 07:30:39 +0000

    soap call added

--- source/main.cpp
+++ source/main.cpp
@@ -7,14 +7,31 @@
 
 auto main(int argc, char* argv[]) -> int
 {
-  std::cout << argc << '\n';
-  std::cout << argv[0] << '\n';
+  if( argc != 4 )
+  {
+    return 0;
+  }
+
+  std::cout << argv[1] << '\n';
+  std::cout << argv[2] << '\n';
+  std::cout << argv[3] << '\n';
+
+  struct soap* soap = soap_new();
 
   ns2__spLogin ns2__LoginRequest;
-  ns2__LoginResponse ns2__LoginResponse_;
+  ns2__LoginRequest.databaseUser = argv[1];
+  ns2__LoginRequest.password = argv[2];
+  ns2__LoginRequest.spUser = argv[3];
+  ns2__LoginRequest.soap = soap;
   
+  ns2__LoginResponse ns2__LoginResponse_;
+
   spProxy proxy;
   proxy.Login(&ns2__LoginRequest, ns2__LoginResponse_);
+
+  soap_destroy(soap);
+  soap_end(soap);
+  soap_free(soap);
 
   return 0;
 }
