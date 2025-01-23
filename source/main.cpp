92f6aebdee6042fa7a8dd147d37f8b9fe1133c25
1poundSOCKS <mathew.coburn@gmail.com>
2025-01-23 08:08:54 +0000

    gsoap 2.8 added to repo

--- source/main.cpp
+++ source/main.cpp
@@ -12,23 +12,24 @@ auto main(int argc, char* argv[]) -> int
     std::cout << argv[arg] << '\n';
   }
 
-  if( argc != 4 )
+  if( argc != 5 )
   {
     return 0;
   }
 
   struct soap* soap = soap_new();
 
+  std::string url = argv[1];
   ns2__spLogin ns2__LoginRequest;
-  ns2__LoginRequest.databaseUser = argv[1];
-  ns2__LoginRequest.password = argv[2];
-  ns2__LoginRequest.spUser = argv[3];
+  ns2__LoginRequest.databaseUser = argv[2];
+  ns2__LoginRequest.password = argv[3];
+  ns2__LoginRequest.spUser = argv[4];
   ns2__LoginRequest.soap = soap;
   
   ns2__LoginResponse ns2__LoginResponse_;
 
   spProxy proxy;
-  std::cout << proxy.Login(&ns2__LoginRequest, ns2__LoginResponse_) << '\n';
+  std::cout << proxy.Login(url.c_str(), "", &ns2__LoginRequest, ns2__LoginResponse_) << '\n';
 
   soap_destroy(soap);
   soap_end(soap);
